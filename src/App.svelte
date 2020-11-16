903d53cd523066b6c46e0a0df59c1f480be18903
Wittner <mike@irelandathome.com>
2020-11-16 22:14:20 +0000

    First commit

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Navbar from "./Navbar.svelte";
+  import Title from "./Title.svelte";
+  let fruits = ["apple", "orange", "lemon"];
+</script>
+
+<Navbar />
+
+{#each fruits as fruit}
+  <Title title={fruit} />
+{/each}
---
+++ src/Title.svelte
@@ -0,0 +1,7 @@
+<script>
+  export let title = "No title set!";
+</script>
+
+<div class="main-title">
+  <h2>{title}</h2>
+</div>
